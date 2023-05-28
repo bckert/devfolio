08bd5c7c9795e9cec145da1ed1143c98db4141ac
angelixa <julia.bockert@outlook.com>
2023-05-28 23:54:00 +0200

    changed theme, added navbar

---
+++ devfolio/src/components/navbar.tsx
@@ -0,0 +1,38 @@
+
+import Image from "next/image";
+import { FunctionComponent } from "react";
+
+const NavBar: FunctionComponent = () => {
+ 
+
+    return (
+      <>
+{/** lägg till fixed om navbaren ska vara fastklistrad */}
+<nav className="bg-green-bg dark:bg-gray-900 w-full z-20 top-0 left-0 dark:border-gray-600 mb-10">
+  <div className="max-w-screen grid justify-items-end p-4">
+
+  <div className="justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+    <ul className="flex flex-col p-4 md:p-0 mt-4 text-2xl border rounded-lg md:flex-row  md:mt-0 md:border-0 md:bg-green-bg dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <li>
+        <a href="#" className="block py-2 pl-3 pr-4 mx-10 text-white rounded hover:text-black" aria-current="page">About</a>
+      </li>
+      <li>
+        <a href="#" className="block py-2 pl-3 pr-4 mx-10 text-white rounded hover:text-black">Portfolio</a>
+      </li>
+      <li>
+        <a href="#" className="block py-2 pl-3 pr-4 mx-10 text-white rounded hover:text-black">Contact</a>
+      </li>
+      <li>
+        <a href="#" className="block py-2 pl-3 pr-4 mx-10 text-white rounded hover:text-black ">CV</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
+
+
+</>
+  );
+};
+
+export default NavBar;
--- devfolio/tailwind.config.ts
+++ devfolio/tailwind.config.ts
@@ -3,7 +3,14 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+          'green-bg': '#305733',
+      },
+      fontFamily: {
+        sans: ['var(--font-dm)'],
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
