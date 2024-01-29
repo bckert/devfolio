4726ce13afd9276ec3bbb2e3f04ee35fe79c3ad8
Julia Böckert <julia.bockert@outlook.com>
2024-01-29 22:18:40 +0100

    tailwind changess

--- devfolio/tailwind.config.ts
+++ devfolio/tailwind.config.ts
@@ -7,7 +7,7 @@ export default {
       colors: {
           'green-bg': '#042F10',
           'brown-bg': '#72310D',
-          'off-white-bg': '#FFF9EF',
+          'off-white-bg': '#F9E9CC',
           'yellow-bg': '#CA903A'
       },
       fontFamily: {
