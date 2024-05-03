a05687dc3bdef06546eaae07548a07843882ece3
Julia Böckert <julia.bockert@outlook.com>
2024-05-03 17:20:34 +0200

    trying to make the pdf responsive

--- devfolio/src/pages/codesign-av-lekplats.tsx
+++ devfolio/src/pages/codesign-av-lekplats.tsx
@@ -6,8 +6,7 @@ const PDFViewer = () => {
  return (
  <>
  <div className="bg-white">
-    <iframe src="Breazeal2004.pdf" className="w-screen h-screen"></iframe>
-    <div className="text-2xl text-black"> HÄR KOMMER PDFEN LIGGA </div>
+    <iframe src="Breazeal2004.pdf" className="lg:w-screen lg:h-screen md:w-screen md-h-screen w-screen h-screen"></iframe>
  </div>
  </> 
  );
