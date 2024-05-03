b17e9962c666aee2e17e8f28796f7c09e25d4d62
Julia Böckert <julia.bockert@outlook.com>
2024-05-03 15:01:32 +0200

    temporary site for a pdf

---
+++ devfolio/src/pages/codesign-av-lekplats.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { type NextPage } from "next";
+import Head from "next/head";
+import React from "react";
+
+const PDFViewer = () => {
+ return (
+ <div>
+       {/* <div className="flex-row-3 ml-20 flex flex-wrap justify-center">
+          {/* <NavBar></NavBar>  <iframe src=”path_to_pdf_file.pdf” width=”100%” height=”500px” /> */}
+ //
+
+ <div className="text-2xl text-white"> HÄR KOMMER PDFEN LIGGA </div>
+ </div>
+ );
+};
+export default PDFViewer;
