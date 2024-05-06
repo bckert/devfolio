0a1c75ca471bd051ea302a30ff9cbde0a92f03dd
Julia Böckert <julia.bockert@outlook.com>
2024-05-06 13:50:30 +0200

    made placeholder links for a friend's thesis

---
+++ devfolio/src/pages/miljoanpassning.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+import React from "react";
+
+//testa med object-fit eller liknande
+const Miljö = () => {
+ return (
+ <>
+
+ 
+ <div className="bg-white lg:w-screen lg:h-screen md:w-screen md-h-screen w-screen h-screen">
+    <iframe src="App_Store_Ethics_The_Game.pdf#page=1&zoom=20"  className="w-full h-full"> </iframe>
+ </div>
+ </> 
+ );
+};
+export default Miljö;
