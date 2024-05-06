578a67b0b2e585406a1751e54a8b226b7fc0d21d
Julia Böckert <julia.bockert@outlook.com>
2024-05-06 14:14:54 +0200

    test scroll

--- devfolio/src/pages/codesign.tsx
+++ devfolio/src/pages/codesign.tsx
@@ -8,7 +8,7 @@ const Codesign = () => {
  <>
 
  
- <div className="bg-white lg:w-screen lg:h-screen md:w-screen md-h-screen w-screen h-screen">
+ <div className="bg-white lg:w-screen lg:h-screen md:w-screen md-h-screen w-full h-screen overflow-scroll">
     <iframe src="App_Store_Ethics_The_Game.pdf#page=1&zoom=20"  className="w-full h-full"> </iframe>
  </div>
  </> 
